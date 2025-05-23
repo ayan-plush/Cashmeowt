3ac6d1fd6399c4fe64154aab6b1fecdddf4f1ebb
ayan-plush <ayankhajuria2005@gmail.com>
2025-05-23 19:33:11 +0530

    fix: add fraud detection via transaction rate

--- controllers/transaction/transactionController.js
+++ controllers/transaction/transactionController.js
@@ -28,6 +28,28 @@ class transactionControllers {
         const { recipientId, amountValue, spendType } = req.body;
         const amount = Number(amountValue)
 
+        const twoHoursAgo = new Date(Date.now() - 2 * 60 * 60 * 1000);
+
+        const recentTransactions = await transactionModel.countDocuments({
+            createdAt: { $gte: twoHoursAgo }
+        })
+
+        if(recentTransactions>5){
+            const fraudUser = await userModel.findByIdAndUpdate(senderId,
+                {
+                    $push: {
+                        fraudFlags: {
+                            reason: "Too many transactions in a short time. Transfer blocked",
+                            date: new Date()
+
+                        }
+                    }
+                },
+                { new: true, session  }
+            )
+            return responseReturn(res,400,{error: "Too many transactions in a short time. Transfer blocked"});
+        }
+
         const session = await mongoose.startSession();
         session.startTransaction();
 
@@ -68,13 +90,14 @@ class transactionControllers {
                 flag
             }], { session });
 
-            const transactionSummary = {
-                id: transaction[0]._id,
-                spendType: transaction[0].spendType,
-                amount: transaction[0].amount,
-                to: recipient.name,
-                from: sender.name
-            };
+            // future implementation of transaction summaries
+            // const transactionSummary = {
+            //     id: transaction[0]._id,
+            //     spendType: transaction[0].spendType,
+            //     amount: transaction[0].amount,
+            //     to: recipient.name,
+            //     from: sender.name
+            // };
 
             // sender.transactions.push(transactionSummary);
             // recipient.transactions.push(transactionSummary);
